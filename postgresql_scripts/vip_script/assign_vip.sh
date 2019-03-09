968b5de00e0e2b92b17cc8635a34e30b0bbb8b9c
sharon <sharonsujee36@gmail.com>
2019-03-09 19:44:02 +0530

    script updated

--- postgresql_scripts/vip_script/assign_vip.sh
+++ postgresql_scripts/vip_script/assign_vip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-. /root/params.conf
+. /root/param.conf
 VIP=$(ifconfig $VIRTUAL_INTERFACE | grep "inet " | awk -F'[: ]+' '{ print $4 }')
-if [ ! -z "$VIP" ]
+if [ -z "$VIP" ]
 then
 vip=$(cat /etc/network/interfaces | grep $VIRTUAL_IP | cut -d ' ' -f 2)
 if [ -z $vip ]
@@ -9,44 +9,47 @@ then
 echo "Virtual IP Configurations Not found in /etc/networks/interface"
 exit
 else
-
 ifup $VIRTUAL_INTERFACE
-
+fi
+fi
 arping -q -c 3 -A -I $VIRTUAL_INTERFACE $VIRTUAL_IP
 
 sleep 5
 VIR_IP=$(ifconfig $VIRTUAL_INTERFACE | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+
 if [ -z "$VIR_IP" ]
 then
-echo " $VIRTUAL_IP Not Asigned in this PC..!!!"
+echo " $VIRTUAL_IP Not Assigned in this Server..!!!"
 else
-echo " $VIRTUAL_IP Asigned Sucess.."
- IP=$(ifconfig $PG_SVR_INTERFACE | grep "inet " | awk -F'[: ]+' '{ print $3 }'| cut -d ' ' -f 2)
+echo " VIP $VIRTUAL_IP Assigned"
+ IP=$(ifconfig $PG_SVR_INTERFACE | grep "inet " | awk -F'[: ]+' '{ print $4 }'| cut -d ' ' -f 2)
      echo $IP
      if [ $IP == $PG_SVR_1_IP ]
      then
-     echo "IP address is equal, assigning server 2 IP as connection IP"
+        echo "IP address is equal, assigning server 2 IP as connection IP"
      CON_IP=$PG_SVR_2_IP
      else
- echo "IP address is not equal, assigning server 1 IP as connection IP"
+        echo "IP address is not equal, assigning server 1 IP as connection IP"
      CON_IP=$PG_SVR_1_IP
      fi
 
-if [ ! -e "$PG_SVR_1_DATA_DIR/recovery.conf" ]; then
-        echo " recovery.conf not found"
-        echo "standby_mode = 'on'" > $PG_SVR_1_DATA_DIR/recovery.conf
-        echo "primary_conninfo = 'host=$CON_IP port=$PG_PORT user=$REP_USER password=$REP_PWD'" >> $PG_SVR_1_DATA_DIR/recovery.conf
-        echo "trigger_file = '/tmp/postgresql.trigger.$PG_PORT'" >> $PG_SVR_1_DATA_DIR/recovery.conf
+        if [ ! -e "$PG_SVR_1_DATA_DIR/recovery.conf" ]; then
+            echo " recovery.conf not found"
+            echo "creating recovery.conf file"
+            echo "standby_mode = 'on'" > $PG_SVR_1_DATA_DIR/recovery.conf
+            echo "primary_conninfo = 'host=$CON_IP port=$PG_PORT user=$REP_USER password=$REP_PWD'" >> $PG_SVR_1_DATA_DIR/recovery.conf
+            echo "trigger_file = '/tmp/postgresql.trigger.$PG_PORT'" >> $PG_SVR_1_DATA_DIR/recovery.conf
+            service postgresql restart
         else
-        echo "recovery.conf found"
-        echo "renaming recovery.conf to recovery.done....."
-        mv $PG_SVR_1_DATA_DIR/recovery.conf $PG_SVR_1_DATA_DIR/recovery.done
-        echo "done"
+            echo "recovery.conf found"
+            echo "renaming recovery.conf to recovery.done....."
+            mv $PG_SVR_1_DATA_DIR/recovery.conf $PG_SVR_1_DATA_DIR/recovery.done
+            echo "shutting down VIP interface"
+            ifdown $VIRTUAL_INTERFACE
+            echo "$VIRTUAL_INTERFACE is down now"
+            echo "done"
+            service postgresql restart
         fi
 fi
-fi
-service postgresql restart
-else
-echo "end block"
-ifdown $VIRTUAL_INTERFACE
-fi
+
+echo "finished"
